c3543e5f2910ed1023c071d6225b9b60933f92a6
Daltonic <darlingtongospel@gmail.com>
2021-03-02 11:18:11 +0100

    Feat: Adds the sidebar and navigational element

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card color="grey lighten-4" flat tile>
+    <v-toolbar dense>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Lyrica</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "MainHeader",
+
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/screens/lyric/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MainHeader />
+  </div>
+</template>
+
+<script>
+import MainHeader from "../../components/MainHeader";
+export default {
+  name: "List",
+
+  components: {
+    MainHeader
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
