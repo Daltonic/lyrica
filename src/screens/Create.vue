76aaa7b494699edc5b9c20a8a169a98cd13c18e2
Daltonic <darlingtongospel@gmail.com>
2021-03-04 13:48:39 +0100

    Update: Adds the CRUD operations

--- src/screens/Create.vue
+++ src/screens/Create.vue
@@ -1,108 +1,111 @@
 <template>
   <div class="my-12">
-    <v-row justify="center">
-      <v-col v-for="(item, i) in 5" :key="i" cols="12">
-        <v-expansion-panels popout>
+    <v-row v-if="notes.length > 0" justify="center">
+      <v-col v-for="(note, i) in notes" :key="i" cols="12">
+        <v-expansion-panels>
           <v-expansion-panel>
-            <v-expansion-panel-header> Item </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </v-expansion-panel-content>
+            <v-expansion-panel-header>{{ note.type }}</v-expansion-panel-header>
+            <v-expansion-panel-content>{{
+              note.text
+            }}</v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
+
         <v-row justify="center" align="center" class="mt-2">
-          <v-btn icon color="indigo">
-              <v-icon>mdi-clipboard-edit</v-icon>
-            </v-btn>
-            <v-btn icon color="pink">
-              <v-icon>mdi-trash-can</v-icon>
-            </v-btn>
+          <v-btn icon color="indigo" @click="editNote(note)">
+            <v-icon>mdi-notebook-edit</v-icon>
+          </v-btn>
+          <v-btn icon color="pink" @click="onRemNote(note)">
+            <v-icon>mdi-trash-can</v-icon>
+          </v-btn>
         </v-row>
       </v-col>
     </v-row>
 
-    <v-row justify="center">
+    <v-row v-else justify="center" align="center">
+      <v-col cols="12" md="8" sm="12">
+        <v-alert border="right" colored-border type="info" elevation="2">
+          You're at the right place, click on the big plus button at the bottom
+          right to start adding notes.
+        </v-alert>
+      </v-col>
+    </v-row>
+
+    <v-form v-model="valid" lazy-validation ref="form" justify="center">
       <v-dialog v-model="dialog" persistent max-width="600px">
         <v-card>
           <v-card-title>
-            <span class="headline">User Profile</span>
+            <span class="headline">Note</span>
           </v-card-title>
           <v-card-text>
             <v-container>
               <v-row>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    label="Legal first name*"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    label="Legal middle name"
-                    hint="example of helper text only on focus"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    label="Legal last name*"
-                    hint="example of persistent helper text"
-                    persistent-hint
-                    required
-                  ></v-text-field>
-                </v-col>
                 <v-col cols="12">
-                  <v-text-field label="Email*" required></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Password*"
-                    type="password"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6">
                   <v-select
-                    :items="['0-17', '18-29', '30-54', '54+']"
-                    label="Age*"
+                    :items="types"
+                    :rules="[(v) => !!v || 'Item is required']"
+                    v-model="note.type"
+                    label="Select The Note Type Here"
                     required
                   ></v-select>
                 </v-col>
-                <v-col cols="12" sm="6">
-                  <v-autocomplete
-                    :items="[
-                      'Skiing',
-                      'Ice hockey',
-                      'Soccer',
-                      'Basketball',
-                      'Hockey',
-                      'Reading',
-                      'Writing',
-                      'Coding',
-                      'Basejump',
-                    ]"
-                    label="Interests"
-                    multiple
-                  ></v-autocomplete>
+
+                <v-col cols="12">
+                  <v-textarea
+                    counter
+                    clearable
+                    v-model="note.text"
+                    clear-icon="mdi-close-circle"
+                    label="Write The Note..."
+                    :rules="[(v) => !!v || 'Note text is required']"
+                    required
+                  ></v-textarea>
                 </v-col>
               </v-row>
             </v-container>
-            <small>*indicates required field</small>
+            <small>All fields are required</small>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="dialog = false">
+            <v-btn type="button" color="blue darken-1" text @click="onClose">
               Close
             </v-btn>
-            <v-btn color="blue darken-1" text @click="dialog = false">
-              Save
+            <v-btn
+              v-if="!edit"
+              type="submit"
+              color="blue darken-1"
+              text
+              @click="addNote"
+            >
+              Add Note
+            </v-btn>
+            <v-btn
+              v-else
+              type="submit"
+              color="blue darken-1"
+              text
+              @click="updateNote"
+            >
+              Update Note
             </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
-    </v-row>
+    </v-form>
+
+    <v-dialog v-model="remDialog" persistent max-width="290">
+      <v-card>
+        <v-card-title class="headline"> Are you sure? </v-card-title>
+        <v-card-text
+          >You are about to delete this "{{ note.type }}"!</v-card-text
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="onClose"> Cancel </v-btn>
+          <v-btn color="pink darken-1" text @click="remNote"> Delete </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
     <v-fab-transition>
       <v-btn
@@ -120,21 +123,85 @@
         <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-fab-transition>
+
+    <v-snackbar v-model="snackbar" timeout="2000">
+      Note successfully deleted!
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    snackbar: false,
     dialog: false,
-    locations: [
-      "Australia",
-      "Barbados",
-      "Chile",
-      "Denmark",
-      "Ecuador",
-      "France",
-    ],
+    remDialog: false,
+    notes: [],
+    types: ["Verse", "Hooks", "Chorus", "Bridge", "Instrument"],
+    note: { text: "", type: "", key: "" },
+    valid: false,
+    edit: false,
   }),
+  methods: {
+    addNote() {
+      if (this.valid) {
+        this.note.key = String(Math.random() * (100 - 1) + 1)
+          .split(".")
+          .join("");
+        this.notes.push({ ...this.note });
+        this.dialog = !this.dialog;
+        this.reset();
+      } else {
+        this.validate();
+      }
+    },
+    updateNote() {
+      if (this.valid) {
+        const index = this.notes.findIndex((i) => i.key == this.note.key);
+        this.notes[index] = { ...this.note };
+        this.notes = [...this.notes];
+        this.dialog = !this.dialog;
+        this.reset();
+      } else {
+        this.validate();
+      }
+    },
+    onRemNote(note) {
+      this.note = { ...note };
+      this.remDialog = !this.remDialog;
+    },
+    remNote() {
+      const index = this.notes.findIndex((i) => i.key == this.note.key);
+      this.notes.splice(index, 1);
+      this.remDialog = !this.remDialog;
+      this.snackbar = !this.snackbar;
+    },
+    editNote(note) {
+      this.edit = true;
+      this.note = { ...note };
+      this.dialog = !this.dialog;
+    },
+    reset() {
+      this.edit = false;
+      this.note.text = "";
+      this.note.type = "";
+      this.note.key = "";
+      this.$refs.form.reset();
+    },
+    onClose() {
+      this.dialog = false;
+      this.remDialog = false;
+      this.reset();
+    },
+    validate() {
+      this.$refs.form.validate();
+    },
+  },
 };
 </script>
