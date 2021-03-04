93af90b0d654de23b7f274be3fd41c7f67065038
Daltonic <darlingtongospel@gmail.com>
2021-03-04 06:53:07 +0100

    Feat: Adds create lyric forms and items

--- src/screens/Create.vue
+++ src/screens/Create.vue
@@ -1,11 +1,140 @@
 <template>
-    <div>
-        <h4>Hello World</h4>
-    </div>
+  <div class="my-12">
+    <v-row justify="center">
+      <v-col v-for="(item, i) in 5" :key="i" cols="12">
+        <v-expansion-panels popout>
+          <v-expansion-panel>
+            <v-expansion-panel-header> Item </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+        <v-row justify="center" align="center" class="mt-2">
+          <v-btn icon color="indigo">
+              <v-icon>mdi-clipboard-edit</v-icon>
+            </v-btn>
+            <v-btn icon color="pink">
+              <v-icon>mdi-trash-can</v-icon>
+            </v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-card>
+          <v-card-title>
+            <span class="headline">User Profile</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal first name*"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Email*" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Password*"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-select
+                    :items="['0-17', '18-29', '30-54', '54+']"
+                    label="Age*"
+                    required
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-autocomplete
+                    :items="[
+                      'Skiing',
+                      'Ice hockey',
+                      'Soccer',
+                      'Basketball',
+                      'Hockey',
+                      'Reading',
+                      'Writing',
+                      'Coding',
+                      'Basejump',
+                    ]"
+                    label="Interests"
+                    multiple
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Close
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+
+    <v-fab-transition>
+      <v-btn
+        key="mdi-plus"
+        color="primary"
+        fab
+        large
+        dark
+        absolute
+        bottom
+        right
+        style="bottom: 100px"
+        @click="dialog = !dialog"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-fab-transition>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  data: () => ({
+    dialog: false,
+    locations: [
+      "Australia",
+      "Barbados",
+      "Chile",
+      "Denmark",
+      "Ecuador",
+      "France",
+    ],
+  }),
+};
 </script>
